96cced051ec52d31dca0a71c71a6a81b052a104a
Aditya-Dev <workchalla98@gmail.com>
2025-11-22 18:05:50 +0530

    Updates

--- lti-spring-rest-flipfit-api-jpa/src/main/java/com/lti/flipfit/LtiSpringRestFlipfitApiJpaApplication.java
+++ lti-spring-rest-flipfit-api-jpa/src/main/java/com/lti/flipfit/LtiSpringRestFlipfitApiJpaApplication.java
@@ -1,11 +1,5 @@
 package com.lti.flipfit;
 
-/**
- * Author :
- * Version : 1.0
- * Description : Main Spring Boot Application class.
- */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
