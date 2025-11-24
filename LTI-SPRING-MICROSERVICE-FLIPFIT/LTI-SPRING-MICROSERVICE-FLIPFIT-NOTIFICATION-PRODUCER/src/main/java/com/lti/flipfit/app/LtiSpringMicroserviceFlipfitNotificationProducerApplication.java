b2ccba0648709f880b38a545e2aefbfc6acebdb0
Aditya-Dev <workchalla98@gmail.com>
2025-11-24 13:54:45 +0530

    Microservice Implementation

---
+++ LTI-SPRING-MICROSERVICE-FLIPFIT/LTI-SPRING-MICROSERVICE-FLIPFIT-NOTIFICATION-PRODUCER/src/main/java/com/lti/flipfit/app/LtiSpringMicroserviceFlipfitNotificationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.lti.flipfit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtiSpringMicroserviceFlipfitNotificationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtiSpringMicroserviceFlipfitNotificationProducerApplication.class, args);
+	}
+
+}
